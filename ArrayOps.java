8d94aaa12c499d01c7dcf4f264599b662eea84c0
Francesca Nemati <FrancescaNemati@Francescas-MBP.lan>
2020-10-22 15:54:07 -0400

    Completed largest method in ArrayOpps and added a simple test case to Tester

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,4 +1,5 @@
 public class ArrayOps {
+
   public static int sum(int[] arr) {
     int sum = 0;
     for (int i = 0; i < arr.length; i++) {
@@ -6,4 +7,13 @@ public class ArrayOps {
     }
     return sum;
   }
+
+  public static int largest(int[] arr) {
+    int largest = 0;
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] > largest) largest = arr[i];
+    }
+    return largest;
+  }
+
 }

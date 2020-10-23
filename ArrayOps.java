cf17ce61fc070a23bb04b6b67ab34321b1945936
Francesca Nemati <FrancescaNemati@Francescas-MBP.lan>
2020-10-23 19:33:24 -0400

    Updated ArrayOps with sumCols, and tested it in Tester

--- ArrayOps.java
+++ ArrayOps.java
@@ -38,6 +38,15 @@ public class ArrayOps {
     return sum;
   }
 
+  public static int[] sumCols(int[][] matrix) {
+    int[] sumCols = new int[matrix[0].length];
+    for (int i = 0; i < matrix[0].length; i++) {
+      for (int j = 0; j < matrix.length; j++) {
+        sumCols[i] += matrix[j][i];
+      }
+    }
+    return sumCols;
+  }
 
   public static String arrToString(int[] arr) {
     String sArr = "{" ;
--- Tester.java
+++ Tester.java
@@ -6,10 +6,13 @@ public class Tester {
     int[][] arr2 = {{1,2,3}, {1,2,4}};
     int[] arr3 = ArrayOps.sumRows(arr2);
     int[] arr4 = ArrayOps.largestInRows(arr2);
+    int[] arr5 = ArrayOps.sumCols(arr2);
     String arrS = ArrayOps.arrToString(arr3);
     String arrS2 = ArrayOps.arrToString(arr4);
+    String arrS3 = ArrayOps.arrToString(arr5);
     System.out.println(arrS);
     System.out.println(arrS2);
     System.out.println(ArrayOps.sum(arr2));
+    System.out.println(arrS3);
   }
 }

4df7f35039ebaa046a204ba1a66a11d71ee3694e
Francesca Nemati <FrancescaNemati@Francescas-MBP.lan>
2020-10-23 19:13:43 -0400

    Updated ArrayOps with largestInRows, and added preliminary test case to Tester

--- ArrayOps.java
+++ ArrayOps.java
@@ -16,4 +16,44 @@ public class ArrayOps {
     return largest;
   }
 
+  public static int[] sumRows(int[][] matrix) {
+    int[] arr_1D = new int[matrix.length];
+    for (int i = 0; i < matrix.length; i++) {
+      arr_1D[i] = sum(matrix[i]);
+    }
+    return arr_1D;
+  }
+
+  public static int[] largestInRows(int[][] matrix) {
+    int[] arr_1D = new int[matrix.length];
+    for (int i = 0; i < matrix.length; i++) {
+      arr_1D[i] = largest(matrix[i]);
+    }
+    return arr_1D;
+  }
+
+
+
+
+  public static String arrToString(int[] arr) {
+    String sArr = "{" ;
+    for (int i = 0; i < arr.length; i++) {
+      if (i != arr.length-1) sArr = sArr + arr[i] + ", ";
+      else sArr = sArr + arr[i];
+    }
+    sArr = sArr + "}";
+    return sArr;
+  }
+
+  public static String arrayDeepToString(int[][] arr) {
+    String sDArr = "{";
+    for (int i = 0; i < arr.length; i++) {
+      if (i != arr.length-1) sDArr = sDArr + arrToString(arr[i]) + ", ";
+      else sDArr = sDArr + arrToString(arr[i]);
+    }
+    sDArr = sDArr + "}";
+    return sDArr;
+  }
+
+
 }
--- Tester.java
+++ Tester.java
@@ -3,5 +3,12 @@ public class Tester {
     int[] arr = {1, 2, 3};
     System.out.println(ArrayOps.sum(arr));
     System.out.println(ArrayOps.largest(arr));
+    int[][] arr2 = {{1,2,3}, {1,2,4}};
+    int[] arr3 = ArrayOps.sumRows(arr2);
+    int[] arr4 = ArrayOps.largestInRows(arr2);
+    String arrS = ArrayOps.arrToString(arr3);
+    String arrS2 = ArrayOps.arrToString(arr4);
+    System.out.println(arrS);
+    System.out.println(arrS2);
   }
 }
